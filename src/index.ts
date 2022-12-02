c99e8317193a78cc34c8f4c0725ea017f0457bd4
Corey <corey@dolomite.io>
2022-12-02 14:33:29 -0500

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from '@dolomite-exchange/sdk-core'
 
-export { FACTORY_ADDRESSES, INIT_CODE_HASHES, MINIMUM_LIQUIDITY } from './constants'
+export { FACTORY_ADDRESSES, INIT_CODE_HASHES, MINIMUM_LIQUIDITY, BalanceCheckFlag } from './constants'
 export * from './errors'
 export * from './entities'
 export * from './router'
